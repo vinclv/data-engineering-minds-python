12d39e627111e0d0c082dd40a38f0bc107f24c5a
Vinod <vinod.chelladuraiv@gmail.com>
2023-06-17 14:07:00 +0200

    Updated README.md with the code link

--- postgres_python/postgres.py
+++ postgres_python/postgres.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
 from faker import Faker
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+LOGGER = logging.getLogger(__name__)
 
 def get_db_engine():
     return create_engine('postgresql://{}:{}@{}/{}'.format('postgres', 'postgres', 'postgres:5432', 'oltp_db'))
@@ -11,18 +15,18 @@ while True:
         if db_engine:
             break
     except Exception as e:
-        print(f"++++ Retrying connection to the database because of the issue {str(e)}++++")
+        LOGGER.warning(f"++++ Retrying connection to the database because of the issue {str(e)}++++")
 
 i = 0
 fake = Faker('en_US')
 #for i in range(10):
 while True:
-    print(f"Inserting record number {i+1}")
+    LOGGER.info(f"Inserting record number {i+1}")
     user_id = fake.random_int(min=1, max=200)
     amount = fake.random_int(min=1000, max=10000)
     insert_query = f"INSERT INTO oltp.user_transactions (user_id, amount) VALUES ({user_id}, {amount})"
     db_engine.execute(text(insert_query).execution_options(autocommit=True))
-    print(f"Completed inserting record number {i+1}")
+    LOGGER.info(f"Completed inserting record number {i+1}")
     i = i + 1
 
 db_engine.close()
