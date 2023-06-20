8be2e1e1ff4c19330ea8a036984602db870b039c
Vinod Chelladurai <47943695+vinclv@users.noreply.github.com>
2023-06-20 16:22:04 +0200

    make user_id not null

--- postgres_python/init.sql
+++ postgres_python/init.sql
@@ -2,7 +2,7 @@ create schema oltp;
 
 
 create table oltp.user_transactions (
-    user_id integer,
+    user_id integer not null,
     amount integer not null,
     created_at timestamp without time zone not null default (current_timestamp at time zone 'utc')
 );
